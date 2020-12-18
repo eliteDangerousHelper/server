dc2972e8f53051405206fca21b10ed7cd695dd15
Cyril POIDEVIN <vilsafur@gmail.com>
2020-12-18 16:05:35 +0100

    feat: add station and system relation to market

--- src/collector/commodity.ts
+++ src/collector/commodity.ts
@@ -3,11 +3,16 @@ import { getRepository } from "typeorm";
 import { Market } from "../entity/Market";
 import { CommodityMarket } from "../entity/CommodityMarket";
 import { Commodity } from "../entity/Commodity";
+import { System } from "../entity/System";
+import { Station } from "../entity/Station";
 
 export default async (message: CommodityMessage) => {
   const marketRepository = getRepository(Market);
   const commodityRepository = getRepository(Commodity);
   const commodityMarketRepository = getRepository(CommodityMarket);
+  const systemRepository = getRepository(System);
+  const stationRepository = getRepository(Station);
+  const prohibitedList = [];
 
   let market = await marketRepository.findOne({
     where: {
@@ -19,8 +24,57 @@ export default async (message: CommodityMessage) => {
   if (!market) {    
     market = new Market();
     market.external_id = message.marketId.toString();
-    await marketRepository.save(market);
   }
+
+  if (market.station === null) {
+    let system = await systemRepository.findOne({
+      where: {
+        name: message.systemName
+      }
+    })
+
+    if (!system) {
+      system = new System();
+      system.name = message.systemName;
+
+      await systemRepository.save(system);
+    }
+    
+    let station = await stationRepository.findOne({
+      where: {
+        name: message.stationName,
+        system
+      }
+    });
+
+    if (!station) {
+      station = new Station()
+      station.name = message.stationName;
+      station.system = system;
+
+      await stationRepository.save(station);
+    }
+
+    market.station = station;
+  }
+
+  for (const p of message.prohibited) {
+    let prohibited = await commodityRepository.findOne({
+      where: {
+        name: p
+      }
+    })
+  
+    if (!prohibited) {
+      prohibited = new Commodity();
+      prohibited.name = p;
+      await commodityRepository.save(prohibited);
+    }
+    prohibitedList.push(prohibited);
+  }
+
+  market.prohibited = prohibitedList;
+  await marketRepository.save(market);
 
   message.commodities.forEach(async c => {
     let commodity = await commodityRepository.findOne({
@@ -46,7 +100,6 @@ export default async (message: CommodityMessage) => {
       commodityMarket.commodity = commodity;
     }
     
-
     commodityMarket.buy_price = c.buyPrice;
     commodityMarket.demand = c.demand;
     commodityMarket.demand_bracket = c.demandBracket === "" ? undefined : c.demandBracket;
@@ -56,6 +109,6 @@ export default async (message: CommodityMessage) => {
     commodityMarket.stock_bracket = c.stockBracket === "" ? undefined : c.stockBracket;
     commodityMarket.update_at = new Date(message.timestamp);
 
-    await commodityMarketRepository.save(commodityMarket).catch(reason => console.log(c));
+    await commodityMarketRepository.save(commodityMarket).catch(reason => console.log(c));    
   })
 }
